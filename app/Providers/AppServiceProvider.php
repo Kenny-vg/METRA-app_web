405e1bb256dfdcc1fe0d74b79eae2ded800f25eb
Aiman Delavega <aimankendra22@gmail.com>
2026-03-01 21:08:59 -0600

    milesima corrección de envio de correos, esperemos que funcione en prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +24,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
-    \URL::forceScheme('https');
-}
+            \URL::forceScheme('https');
+        }
+
+        Mail::extend('brevo', function (array $config) {
+            return (new BrevoTransportFactory)->create(
+                new Dsn('brevo+api', 'default', config('services.brevo.key'))
+            );
+        });
     }
+
 }
